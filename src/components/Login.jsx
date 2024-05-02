19d210b308f0c7b521e068685aa06e35e2055ffc
sammy <kalindasammy4.com>
2024-05-02 12:15:39 +0200

    first commit 2

---
+++ src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        {/* header */}
+        <div className='bg-black text-white flex pl-80 items-center py-3'>
+            <div>
+                <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+ <a href='google.com' className='font-bold decoration-1'>ShopNow</a></p>
+            </div>
+            <div className='ml-60'>
+                <select name="" id=""  className='bg-transparent'>
+                    <option value="English" >English</option>
+                </select>
+            </div>
+        </div>
+
+
+        {/* body */}
+        <div>
+
+        </div>
+
+
+        {/* footer */}
+
+
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
