a8b5a82277067911c1ab1a42b278e85ef33a7ee3
sammy002621 <kalindasammy4@gmail.com>
2024-05-23 00:03:58 +0200

    first day commit

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './Header'
 import HeaderHome from './HeaderHome'
 import Footer from './Footer'
+import { Link } from 'react-router-dom'
 
 function PageNotFound() {
   return (
     <div>
         <Header/>
         <HeaderHome/>
-        <div className='flex mx-5 lg:mx-20 2xl:mx-80 lg:mt-20 mt-10 mb-10   '>
+        <div className='flex mx-5 lg:mx-10 2xl:mx-80 lg:mt-20 mt-10 mb-10   '>
           <div className='flex   justify-around '>
           <div className='text-gray-600'>Home</div>
           <div className='mx-2'>/</div>
@@ -20,7 +21,7 @@ function PageNotFound() {
     <div className='md:mt-20 md:pt-10 mt-10'>
         <h1 className='mb-5 lg:text-3xl lg:font-semibold text-3xl font-medium '>404 Not Found</h1>
         <p className='mb-10 font-light text-sm'>Your visited page not found. You may go home page.</p>
-        <button className='bg-sammy text-white px-5 py-2 rounded-sm font-semibold text-xl'>Back to home page</button>
+        <Link className='bg-sammy text-white px-5 py-2 rounded-sm font-semibold text-xl' to={'/'}>Back to homepage</Link>
     </div>
    </div>
 
