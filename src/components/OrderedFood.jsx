19198234f097c526bba744e5f13208336aa9ba29
sammy002621 <kalindasammy4@gmail.com>
2024-06-17 13:26:56 +0200

    adjusted the most orderd food section

--- src/components/OrderedFood.jsx
+++ src/components/OrderedFood.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function OrderedFood({id,name,image,price}) {
   return (
-    <div key={id} className='relative'>
+    <div key={id} className='flex justify-around relative mb-10 items-start'>
 <div>
-<img src={image} alt={name} className='absolute ' />
+<img src={image} alt={name} className='absolute left-0 ' />
 </div>
 <p>{name}</p>
 <p>{price}</p>
